04e40fa3ef903a550506dc081d522294e8979336
Vitor Bertazzo <vbertazzo@gmail.com>
2020-06-03 19:26:50 -0300

    test: ensure Login starts with submit button disabled

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
 import Login from './login'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 
 describe('Login Component', () => {
-  test('Should not render spinner and error on start', () => {
+  test('Should start with initial state', () => {
     render(<Login />)
     const errorWrap = screen.getByRole('status')
     expect(errorWrap.childElementCount).toBe(0)
+    const submitButton = screen.getByRole('button', { name: /Entrar/i })
+    expect(submitButton).toBeDisabled()
   })
 })
